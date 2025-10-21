294e4e7bbed00c52bcb84463e16731e2b604845a
fab-pato05 <fab-pato05@example.com>
2025-10-21 11:40:54 -0600

    Cambios base de datos

--- db.js
+++ db.js
@@ -1,18 +1,33 @@
 // db.js
-const { Pool } = require('pg');
+//const { Pool } = require('pg');
 
 // Configuración de conexión
-const pool = new Pool({
-    user: 'postgres',        //  usuario de PostgreSQL
-    host: 'localhost',       // IP del servidor
-    database: 'circulo_seguro',  // nombre de  base de datos
-    password: '1234', // la contraseña del usuario postgres
-    port: 5432,              // puerto por defecto
-});
+//const pool = new Pool({
+//user: 'postgres',        //  usuario de PostgreSQL
+//host: 'localhost',       // IP del servidor
+//database: 'circulo_seguro',  // nombre de  base de datos
+//password: '1234', // la contraseña del usuario postgres
+//port: 5432,              // puerto por defecto
+//});
 
 // Probar la conexión
-pool.connect()
-    .then(() => console.log('✅ Conectado a PostgreSQL correctamente'))
-    .catch(err => console.error('❌ Error de conexión:', err));
+//pool.connect()
+//.then(() => console.log('✅ Conectado a PostgreSQL correctamente'))
+//.catch(err => console.error('❌ Error de conexión:', err));
 
-module.exports = pool;
+//module.exports = pool;
+// db.js
+import pkg from 'pg';
+const { Pool } = pkg;
+
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: 5432,
+    ssl: { rejectUnauthorized: false }
+});
+
+export default pool;
+
