e174855634b706904081741eb4f049444a928b4a
fab-pato05 <fab-pato05@example.com>
2025-10-29 14:40:38 -0600

    caambios

--- Views/Js/cotizador.js
+++ Views/Js/cotizador.js
@@ -1,22 +1,40 @@
-document.getElementById("cotizacionForm").addEventListener("submit", function (e) {
+document.getElementById("cotizacionForm").addEventListener("submit", async function (e) {
     e.preventDefault();
-    const monto = parseFloat(this.monto.value);
+
+    const formData = new FormData(this);
+    const data = Object.fromEntries(formData.entries());
+    const monto = parseFloat(data.monto_asegurar);
+
     if (isNaN(monto) || monto < 10000) {
         alert("El monto a asegurar debe ser mayor a $10,000");
         return;
     }
 
-    // Aquí podrías enviar los datos a tu servidor o API
-    alert("Formulario enviado con éxito ✅");
-    this.reset();
-});
-        document.getElementById("cotizacionForm").addEventListener("submit", function (e) {
-            e.preventDefault();
-            const formData = new FormData(this);
-            const data = Object.fromEntries(formData.entries());
-
-            localStorage.setItem("cotizacion", JSON.stringify(data));
-
-            alert(`Has seleccionado la póliza: ${data.tipoPoliza}`);
-            window.location.href = "contratar.html"; // redirige a la siguiente página
+    try {
+        const res = await fetch("http://localhost:3000/guardar-cotizacionForm", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data)
         });
+
+        const text = await res.text();
+        alert(text);
+        this.reset();
+    } catch (err) {
+        console.error(err);
+        alert("❌ Error al enviar cotización");
+    }
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
